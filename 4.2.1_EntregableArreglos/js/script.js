8cd618c0aaed706ae0a98f5123c10f67335d3c91
Leviathan <l23920238@voaxaca.tecnm.mx>
2025-05-11 20:13:41 -0600

    Entregable de Estructura de Datos

---
+++ 4.2.1_EntregableArreglos/js/script.js
@@ -0,0 +1,49 @@
+
+const listaDeCompras = [];  // Lista vacía
+
+// Función para agregar un producto
+function agregarProducto() {
+  let producto = document.getElementById("producto").value; // Obtener el valor del input
+  let cantidad = document.getElementById("cantidad").value; // Obtener el valor del input de cantidad
+  if (listaDeCompras.includes(producto)) { // Verificar si el producto ya está en la lista
+    console.log("El producto ya está en la lista.");
+    alert("El producto ya está en la lista.");
+  }else if (producto === "") { // Verificar si el input está vacío
+    alert("Por favor, ingresa un producto.");
+    console.log("Por favor, ingresa un producto.");
+  }else {
+    listaDeCompras.push(producto); // Agregar el producto a la lista
+    console.log("Producto agregado: " + producto);
+  }
+  document.getElementById("producto").value = ""; // Limpiar el input
+}
+// Función para mostrar la lista completa
+function mostrarLista() {
+  console.log("Lista de compras:");
+  for (let i = 0; i < listaDeCompras.length; i++) {
+    console.log("- " + listaDeCompras[i]);
+  }
+}
+
+let agregarProductoBtn = document.getElementById("agregarProductoBtn");
+agregarProductoBtn.addEventListener("click", agregarProducto);
+
+let mostrarListaBtn = document.getElementById("mostrarListaBtn");
+mostrarListaBtn.addEventListener("click", mostrarLista);
+
+// Prueba tus funciones
+
+/*
+agregarProducto("Leche");
+agregarProducto("Pan");
+agregarProducto("Leche");  // Este producto no debe agregarse de nuevo
+*/
+
+//mostrar la lista actualizada en listaProductos
+function mostrarLista() {
+  let listaCompleta = "Lista de compras:\n";
+  for (let i = 0; i < listaDeCompras.length; i++) {
+    listaCompleta += "- " + listaDeCompras[i] + "\n";
+  }
+  document.getElementById("listaProductos").value = listaCompleta; // Mostrar la lista en el listaProductos
+}
---
+++ 4.2.1_EntregableArreglos/readme.md
@@ -0,0 +1,11 @@
+# Lista de compras
+
+-Este proyecto ha utilizado el código proporcionado del documento de Google Drive para poderse realizar
+
+## modificaciones
+
+-Se han realizado las modificaciones solicitadas y se ha agregado el código correspondiente, se han hecho modificaciones adicionales para que el usuario ingrese los datos
+
+# Integrantes del equipo
+
+-Este proyecto ha sido realizado por Efraín Miguel Medina
